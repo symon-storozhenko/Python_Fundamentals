5c374d4024bb644b06c6f3c1979557c7e375d575
symonstorozhenko <stosimon@gmail.com>
2021-06-27 22:04:39 -0400

    added first_program.py and strings_integers_floats.py

---
+++ strings_integers_floats.py
@@ -0,0 +1,13 @@
+# string -> "Heelo", 'Hello', "Heelo, 123", "123!?/']"
+
+print("Hello, X AE A-Xii. Today is Dec 31st.")
+print('9+1')
+print('1.5 +1')
+
+# integer -> 1, 2, 3, 5
+
+print(9+1)
+
+# float -> 2.5, 3.785
+
+print(1.5+1)
