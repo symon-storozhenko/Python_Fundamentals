aaa6c9d50bc21abb07df32d9621beba626362107
symonstorozhenko <stosimon@gmail.com>
2021-06-28 22:22:50 -0400

    added f-strings.py

--- f-strings.py
+++ f-strings.py
@@ -1,18 +1,4 @@
-a = 3
 greeting = "Hello,"
-name = "Zoey"
-
-
-print(a + 3)
-
-print(greeting + ", there!")
-
-print(greeting + " " + name)
-
-name = "Zoeski"
-
-print(greeting + " " + name)
-
 name = "Zoey"
 
 print(greeting + " " + name)
@@ -21,12 +7,8 @@ print(f'{greeting} {name}')
 
 full_greeting = f'{greeting} {name}'
 
-print(f'{greeting} (printed with f-string)')
+print(f'{full_greeting}!')
 
-
-user_input = input("Type you name here:")
-
-print(user_input)
 
 
 
