dde5993741ea73f843735c8cf154ed83fffcb7be
symonstorozhenko <stosimon@gmail.com>
2021-07-10 10:46:29 -0400

    added elif_statements.py and boolean_logic.py

---
+++ boolean_logic.py
@@ -0,0 +1,14 @@
+a = 3
+b = 4
+c = 5
+
+#  and/or
+# print(a == 3 and b == 4 and c == 5)
+print(a == 3 or (b == 44 and c == 55))
+
+
+#
+# print(True and True)
+# print(f'True and False -> {True and False}')
+# print(False and False)
+# print(True or False)
---
+++ elif_statements.py
@@ -0,0 +1,13 @@
+a = 3
+
+if a < 10:
+    print("3 is less than 10")
+elif a > 10:
+    print("3 is greater than 10")
+elif a == 3:
+    print("3 is equal to 3")
+else:
+    print("The error!")
+# Tab or 4 spaces make an indentation
+
+print("The end of program")
